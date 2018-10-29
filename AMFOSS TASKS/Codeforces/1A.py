1815e895df30d3f22d532a47f2f1c526ed6918b8
Freakston <kssuraj15@gmail.com>
2018-10-29 18:53:38 +0530

    Fixed runtime error

--- AMFOSS TASKS/Codeforces/1A.py
+++ AMFOSS TASKS/Codeforces/1A.py
@@ -1,13 +1,11 @@
 #!python3
 import math
-b=input()
-n =int(b[0])
-m =int(b[2])
-a =int(b[4])
+n,m,a =map(int, (input().split(" ")))
+
 if (1 <= n <= 10**9):
         if (1 <= m <= 10**9):
             if (1 <= a <= 10**9):
                 rows =math.ceil(n/a)
-                column =math.ceil(m/a)
-                final =rows*column
+                column = math.ceil(m/a)
+                final = rows*column
 print(final)
